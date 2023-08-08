0b413827881b09f632fc3a31e8acc0e7cd15fb66
min-0 <dhe08133@naver.com>
2023-08-08 14:50:13 +0900

    fix: WiFi Connect, LCD Emotion

--- smartfarm/smartfarm.ino
+++ smartfarm/smartfarm.ino
@@ -1,13 +1,13 @@
+#include <Emotion_Farm.h>
 #include <Servo.h>
 #include <DHT.h>
 #include <PubSubClient.h>
-#include "WiFiEsp.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <String.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
+#include "WiFiEsp.h"
 #define DHTPIN 12
 #define DHTTYPE DHT11
 #define SERVOPIN 9
@@ -26,7 +26,7 @@ String topicStr = "";
 float temperature, humidity;
 int angle = 0;
 int get_co2_ppm = 0;
-int cdcValue = 0;
+int cdsValue = 0;
 int waterValue = 0;
 int lightOutput = 0;
 int fanOutput = 0;
@@ -36,7 +36,6 @@ int send_time = 0;
 bool water_State = false;
 unsigned water_Time = 0;
 unsigned local_time = 0;
-
 char sData[64] = {
   0x00,
 };
@@ -46,27 +45,24 @@ char rData[32] = {
 char nData[32] = {
   0x00,
 };
-
 int rPos = 0;
 int nPos = 0;
 int right = 10;
-int displayToggle = 1;
-
+int displayToggle = 0;
 static bool isLed = true;
 static bool isPump = false;
 static bool isFan = false;
 static bool isWindow = false;
 
-char ssid[] = "min0_5G";
+char ssid[] = "min0";
 char pass[] = "12345678";
-
 int status = WL_IDLE_STATUS;
 WiFiEspServer server_f(400);
 WiFiEspClient espClient;
-PubSubClient client(espClient);
 
+PubSubClient client(espClient);
 const char* clientName = "";
-const char* mqtt_server = "129.254.174.129";
+const char* mqtt_server = "129.254.174.120";
 
 DHT dht(DHTPIN, DHTTYPE);
 Servo servo;
@@ -79,26 +75,32 @@ void printLCD(int col, int row, char* str) {
   }
 }
 
-void printWifiStatus() {
+void printWifiStatus(){
+#if 1 // #if DEBUG
   // print the SSID of the network you're attached to
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
+#endif
 
   // print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   delay(10);
+#if 1 // #if DEBUG
   Serial.print("IP Address: ");
   Serial.println(ip);
-  char ipno2[26];
-  sprintf(ipno2, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-  printLCD(0, 1, ipno2);
+#endif
+	char ipno2[26] ;
+	sprintf(ipno2, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  	printLCD(0, 1, ipno2);
 
   // print the received signal strength
   long rssi = WiFi.RSSI();
-
+  
+#if 1 // #if DEBUG
   Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+#endif
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -110,10 +112,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
-
   servo.attach(SERVOPIN);
   servo.detach();
-  if(topicStr == "farm/actuator/window"){
+  if(topicStr == "smart/farm/actuator/window"){
     Serial.println("Window!");
     // Switch on the LED if an 1 was received as first character
     if ((char)payload[0] == '1') angle = 10;
@@ -121,10 +122,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
         servo.attach(SERVOPIN);
         servo.write(angle);
   }
-
-  if(topicStr == "farm/actuator/pump"){
+  if(topicStr == "smart/farm/actuator/pump"){
     Serial.println("WaterPump!");
-
     if ((char)payload[0] == '1') {
       digitalWrite(WATER_PUMP_PIN, 1);
       water_State = true;
@@ -147,10 +146,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
       water_Time = 0;
       isPump = water_State;
     }
-    
   }
-
-  if(topicStr == "farm/actuator/led"){
+  if(topicStr == "smart/farm/actuator/led"){
      if ((char)payload[0] == '1') {
       analogWrite(LIGHTPIN, 255);
       isLed = true;
@@ -160,7 +157,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
        isLed = false;
      }
   }
-  if(topicStr == "farm/actuator/fan"){
+  if(topicStr == "smart/farm/actuator/fan"){
     if ((char)payload[0] == '1') {
       digitalWrite(FAN_PIN, 1);
       isFan = true;
@@ -170,55 +167,67 @@ void callback(char* topic, byte* payload, unsigned int length) {
     }
   }
 }
-
-
-void subscribeMQTT() {      
-  client.subscribe("farm/actuator/window", MQTTsubQos); // 창문
-  client.subscribe("farm/actuator/pump", MQTTsubQos); // 물펌프
-  client.subscribe("farm/actuator/led", MQTTsubQos); // 조명
-  client.subscribe("farm/actuator/fan", MQTTsubQos); // 환기팬
+void subscribeMQTT() {
+  client.subscribe("smart/farm/actuator/window", MQTTsubQos); // 창문
+  client.subscribe("smart/farm/actuator/pump", MQTTsubQos); // 물펌프
+  client.subscribe("smart/farm/actuator/led", MQTTsubQos); // 조명
+  client.subscribe("smart/farm/actuator/fan", MQTTsubQos); // 환기팬
 }
 
 void reconnect() {
   Serial.print("Attempting MQTT connection...");
-  
-  client.setServer(mqtt_server, 1883); 
+  client.setServer(mqtt_server, 1883);
     if (client.connect(mqtt_server)) {
       subscribeMQTT();
       Serial.println("Subscribe!");
     }
-  
   client.setCallback(callback);
 }
 
 void setup_wifi() {
-  status = WiFi.status();
-  while (status != WL_CONNECTED) {
+  #if USE_NETWORK
+  // initialize serial for ESP module
+  Serial2.begin(9600);
+  // initialize ESP module
+  WiFi.init(&Serial2);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+  	#if DEBUG
+    Serial.println("WiFi shield not present");
+	#endif
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+  	#if DEBUG
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-    Serial.print("Status: ");
-    Serial.println(status);
-
+	#endif
+    // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, pass);
-    if (status != WL_CONNECTED) {
-      Serial.println("Connect Fail - call wait status");
-
-      delay (100);
-    } else {
-      delay (1000);
-    }
   }
+	#if DEBUG
+  Serial.println("You're connected to the network");
+	#endif
+  printWifiStatus(); // display IP address on LCD
+  delay(2000);
+	
+  server_f.begin(); 
+#endif
+  
+  #if DEBUG
+  Serial.println("START");
+  #endif
 }
 
-// ------------------------------------------------------------------------------------------
-
 void setup() {
-
   pinMode(LIGHTPIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
   pinMode(WATER_PUMP_PIN, OUTPUT);
   servo.attach(SERVOPIN);
-
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial2.begin(9600);
@@ -227,38 +236,36 @@ void setup() {
   while (!Serial) {
     ;  // wait for serial port to connect. Needed for native USB port only
   }
-
-  lcd.init();
+  lcd.begin();
   lcd.backlight();
-  printLCD(0, 0, "MMB_SmartFarm");
-  printLCD(0, 1, "NETWORKING...");
+  printLCD(0, 0, "START SMART FARM");
+  printLCD(0, 1, "CONNECTING...");
 
-  WiFi.init(&Serial2);
+  lcd.createChar(0, temp);
+  lcd.createChar(1, C);
+  lcd.createChar(2, humi);
+  lcd.createChar(3, Qmark);
+  lcd.createChar(4, water);
+  lcd.createChar(5, good);
+  lcd.createChar(6, wind);
+  lcd.createChar(7, per);
+
   setup_wifi();
-  // check for the presence of the shield
-  Serial.println("You're connected to the network");
-
-  printWifiStatus();  // display IP address on LCD
-
-  Serial.println("START");
-
-  client.setServer(mqtt_server, 1883); 
+  //check for the presence of the shield
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   Serial.println("Callback!");
   if (client.connect(clientName)) {
-    Serial.println("MQTT Clinet Connect!");
-    subscribeMQTT();
-    Serial.println("Subscribe!");
+   Serial.println("MQTT Clinet Connect!");
+   subscribeMQTT();
+   Serial.println("Subscribe!");
   }
-  Serial.println("START!");
-
 }
 char tem[20];
 char hum[20];
 char illuminance[20];
 char soilhumidity[20];
 char pubJson[512];
-
 void loop() {
   if (!client.connected()) {
     Serial.println("Mqtt Client Not Connected");
@@ -268,23 +275,21 @@ void loop() {
 
   utoa(temperature, tem, 10);
   utoa(humidity, hum, 10);
-  utoa(cdcValue, illuminance, 10);
+  utoa(cdsValue, illuminance, 10);
   utoa(waterValue, soilhumidity, 10);
-
-  sprintf(pubJson, "{\"Sensor\":{\"temperature\":%s,\"humidity\":%s,\"illuminance\":%s,\"soilhumidity\":%s},\"Actuator\":{\"led\":%d,\"fan\":%d,\"pump\":%d,\"window\":%d}}", 
+  sprintf(pubJson, "{\"Sensor\":{\"temperature\":%s,\"humidity\":%s,\"illuminance\":%s,\"soilhumidity\":%s},\"Actuator\":{\"led\":%d,\"fan\":%d,\"pump\":%d,\"window\":%d}}",
            tem, hum, illuminance, soilhumidity,isLed, isFan, isPump, isWindow );
-
+  
   send_time += 1;
   if ((send_time) > 10) {
     Serial.println("Sensor Timer is ON");
-    client.publish("farm/message", pubJson, MQTTpubQos);
-
+    client.publish("smart/farm/message", pubJson, MQTTpubQos);
     Serial.print("temperature : ");
     Serial.println(temperature);
     Serial.print("humidity : ");
     Serial.println(humidity);
     Serial.print("illuminance : ");
-    Serial.println(cdcValue);
+    Serial.println(cdsValue);
     Serial.print("soilhumidity : ");
     Serial.println(waterValue);
     send_time = 0;
@@ -292,50 +297,72 @@ void loop() {
 
   timeout += 1;
   if (timeout % 10 == 0) {
-
-    cdcValue = analogRead(0);
-    cdcValue /= 10;
-
+    cdsValue = analogRead(0);
+    cdsValue /= 10;
     waterValue = analogRead(1);
     waterValue /= 10;
-
     humidity = dht.readHumidity();
     temperature = dht.readTemperature();
-
+    
     lcd.clear();
     displayToggle = !displayToggle;
     if (displayToggle == 1) {
-      memset(sData, 0x00, 64);
-      sprintf(sData, "temp %02dC humi %02d%%", (int)temperature,
-              (int)humidity);
-      printLCD(0, 0, sData);
-      memset(sData, 0x00, 64);
-      sprintf(sData, "cdc%-04d soil%-04d", cdcValue, waterValue);
-      printLCD(0, 1, sData);
+      lcd.setCursor(0, 0);
+      lcd.write(5);
+      sprintf(sData, "WELCOME TO FARM");
+      lcd.print(sData);
+      lcd.setCursor(0, 1);
+      sprintf(sData, "FARM WORKING...");
+      lcd.print(sData);
+      lcd.write(3);
+      
     } else {
-      memset(sData, 0x00, 64);
-      sprintf(sData, "temp %02dC humi %02d%%", (int)temperature,
-              (int)humidity);
-      printLCD(0, 0, sData);
-      memset(sData, 0x00, 64);
-      sprintf(sData, "co2 %d ppm", get_co2_ppm);
-      printLCD(0, 1, sData);
-    }
+      //온도
+      lcd.setCursor(0, 0);
+      lcd.write(0);
+      lcd.print(":");
+      sprintf(sData, "%02d", (int)temperature);
+      lcd.print(sData);
+      lcd.write(1);
 
-    sprintf(sData, "{ \"temp\":%02d,\"humidity\":%02d,\"cdc\":%-04d,\"water\":%-04d,\"co2\":%-04d }",
+      //토양습도
+      lcd.setCursor(6, 0);
+      lcd.write(2);
+      lcd.print(":");
+      sprintf(sData, "%02d", (int)humidity);
+      lcd.print(sData);
+      lcd.write(7);
+
+      //조도
+      lcd.setCursor(12, 0);
+      lcd.write(6);
+      lcd.print(":");
+      sprintf(sData, "%02d", cdsValue);
+      lcd.print(sData);
+      
+      //습도
+      lcd.setCursor(0, 1);
+      lcd.write(4);
+      lcd.print(":");
+      sprintf(sData, "%02d", waterValue);
+      lcd.print(sData);
+      lcd.write(7);
+
+      //이산화탄소
+      lcd.setCursor(6, 1);
+      sprintf(sData, "co2: %02dppm", get_co2_ppm);
+      lcd.print(sData);
+    }
+    sprintf(sData, "{ \"temp\":%02d,\"humidity\":%02d,\"cds\":%-04d,\"water\":%-04d,\"co2\":%-04d }",
             (int)temperature, (int)humidity,
-            cdcValue, waterValue, get_co2_ppm);
+            cdsValue, waterValue, get_co2_ppm);
     Serial1.println(sData);
   }
-
-// 0 이면 farm/sensor/status -> 센서1,상태 센서2,상태
-
 
   if (WiFi.status() != WL_CONNECTED) {
     setup_wifi();
     reconnect();
     Serial.println("reconnect!");
   }
-
   delay(1000);
 }
