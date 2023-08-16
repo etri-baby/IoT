026d50adf074e999a651c8f549bd9b8bd357f59e
min-0 <dhe08133@naver.com>
2023-08-16 16:52:22 +0900

    feat: RTC module 추가

--- smartfarm/smartfarm.ino
+++ smartfarm/smartfarm.ino
@@ -1,3 +1,4 @@
+#include <RtcDS3231.h>
 #include <Emotion_Farm.h>
 #include <Servo.h>
 #include <DHT.h>
@@ -67,6 +68,7 @@ const char* mqtt_server = "129.254.174.120";
 DHT dht(DHTPIN, DHTTYPE);
 Servo servo;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+RtcDS3231<TwoWire> Rtc(Wire);
 
 void printLCD(int col, int row, char* str) {
   for (int i = 0; i < strlen(str); i++) {
@@ -75,8 +77,8 @@ void printLCD(int col, int row, char* str) {
   }
 }
 
-void printWifiStatus(){
-#if 1 // #if DEBUG
+void printWifiStatus() {
+#if 1  // #if DEBUG
   // print the SSID of the network you're attached to
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -85,18 +87,18 @@ void printWifiStatus(){
   // print your WiFi shield's IP address
   IPAddress ip = WiFi.localIP();
   delay(10);
-#if 1 // #if DEBUG
+#if 1  // #if DEBUG
   Serial.print("IP Address: ");
   Serial.println(ip);
 #endif
-	char ipno2[26] ;
-	sprintf(ipno2, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-  	printLCD(0, 1, ipno2);
+  char ipno2[26];
+  sprintf(ipno2, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  printLCD(0, 1, ipno2);
 
   // print the received signal strength
   long rssi = WiFi.RSSI();
-  
-#if 1 // #if DEBUG
+
+#if 1  // #if DEBUG
   Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
@@ -114,22 +116,22 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
   servo.attach(SERVOPIN);
   servo.detach();
-  if(topicStr == "smart/farm/actuator/window"){
+  if (topicStr == "smart/farm/actuator/window") {
     Serial.println("Window!");
     // Switch on the LED if an 1 was received as first character
     if ((char)payload[0] == '1') angle = 10;
-        else angle = 80;
-        servo.attach(SERVOPIN);
-        servo.write(angle);
+    else angle = 80;
+    servo.attach(SERVOPIN);
+    servo.write(angle);
   }
-  if(topicStr == "smart/farm/actuator/pump"){
+  if (topicStr == "smart/farm/actuator/pump") {
     Serial.println("WaterPump!");
     if ((char)payload[0] == '1') {
       digitalWrite(WATER_PUMP_PIN, 1);
       water_State = true;
       isPump = water_State;
-      if(water_State){
-        for(int i = 0; i < 2500; i++) {
+      if (water_State) {
+        for (int i = 0; i < 2500; i++) {
           water_Time += 1;
         }
         if ((water_Time) > 2500) {
@@ -139,25 +141,23 @@ void callback(char* topic, byte* payload, unsigned int length) {
           isPump = water_State;
         }
       }
-    }
-    else {
+    } else {
       digitalWrite(WATER_PUMP_PIN, 0);
       water_State = false;
       water_Time = 0;
       isPump = water_State;
     }
   }
-  if(topicStr == "smart/farm/actuator/led"){
-     if ((char)payload[0] == '1') {
+  if (topicStr == "smart/farm/actuator/led") {
+    if ((char)payload[0] == '1') {
       analogWrite(LIGHTPIN, 255);
       isLed = true;
-     }
-     else{
-       analogWrite(LIGHTPIN, 0);
-       isLed = false;
-     }
+    } else {
+      analogWrite(LIGHTPIN, 0);
+      isLed = false;
+    }
   }
-  if(topicStr == "smart/farm/actuator/fan"){
+  if (topicStr == "smart/farm/actuator/fan") {
     if ((char)payload[0] == '1') {
       digitalWrite(FAN_PIN, 1);
       isFan = true;
@@ -168,24 +168,24 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 }
 void subscribeMQTT() {
-  client.subscribe("smart/farm/actuator/window", MQTTsubQos); // 창문
-  client.subscribe("smart/farm/actuator/pump", MQTTsubQos); // 물펌프
-  client.subscribe("smart/farm/actuator/led", MQTTsubQos); // 조명
-  client.subscribe("smart/farm/actuator/fan", MQTTsubQos); // 환기팬
+  client.subscribe("smart/farm/actuator/window", MQTTsubQos);  // 창문
+  client.subscribe("smart/farm/actuator/pump", MQTTsubQos);    // 물펌프
+  client.subscribe("smart/farm/actuator/led", MQTTsubQos);     // 조명
+  client.subscribe("smart/farm/actuator/fan", MQTTsubQos);     // 환기팬
 }
 
 void reconnect() {
   Serial.print("Attempting MQTT connection...");
   client.setServer(mqtt_server, 1883);
-    if (client.connect(mqtt_server)) {
-      subscribeMQTT();
-      Serial.println("Subscribe!");
-    }
+  if (client.connect(mqtt_server)) {
+    subscribeMQTT();
+    Serial.println("Subscribe!");
+  }
   client.setCallback(callback);
 }
 
 void setup_wifi() {
-  #if USE_NETWORK
+#if USE_NETWORK
   // initialize serial for ESP module
   Serial2.begin(9600);
   // initialize ESP module
@@ -193,34 +193,79 @@ void setup_wifi() {
 
   // check for the presence of the shield
   if (WiFi.status() == WL_NO_SHIELD) {
-  	#if DEBUG
+#if DEBUG
     Serial.println("WiFi shield not present");
-	#endif
+#endif
     // don't continue
-    while (true);
+    while (true)
+      ;
   }
 
   // attempt to connect to WiFi network
-  while ( status != WL_CONNECTED) {
-  	#if DEBUG
+  while (status != WL_CONNECTED) {
+#if DEBUG
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
-	#endif
+#endif
     // Connect to WPA/WPA2 network
     status = WiFi.begin(ssid, pass);
   }
-	#if DEBUG
+#if DEBUG
   Serial.println("You're connected to the network");
-	#endif
-  printWifiStatus(); // display IP address on LCD
-  delay(2000);
-	
-  server_f.begin(); 
 #endif
-  
-  #if DEBUG
+  printWifiStatus();  // display IP address on LCD
+  delay(2000);
+
+  server_f.begin();
+#endif
+
+#if DEBUG
   Serial.println("START");
-  #endif
+#endif
+}
+
+void setup_rtc() {
+  Serial.begin(115200);
+  Serial.print("compiled: ");
+  Serial.print(__DATE__);
+  Serial.println(__TIME__);
+
+  //--------RTC SETUP ------------
+  // if you are using ESP-01 then uncomment the line below to reset the pins to
+  // the available pins for SDA, SCL
+  // Wire.begin(0, 2); // due to limited pins, use pin 0 and 2 for SDA, SCL
+
+  Rtc.Begin();
+#if defined(WIRE_HAS_TIMEOUT)
+  Wire.setWireTimeout(3000 /* us */, true /* reset_on_timeout */);
+#endif
+
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+
+  if (!Rtc.IsDateTimeValid()) {
+    Serial.println("RTC lost confidence in the DateTime!");
+
+    // following line sets the RTC to the date & time this sketch was compiled
+    // it will also reset the valid flag internally unless the Rtc device is
+    // having an issue
+
+    Rtc.SetDateTime(compiled);
+  }
+
+  if (!Rtc.GetIsRunning()) {
+    Serial.println("RTC was not actively running, starting now");
+    Rtc.SetIsRunning(true);
+  }
+
+  RtcDateTime now = Rtc.GetDateTime();
+  if (now < compiled) {
+    Serial.println("RTC is older than compile time, updating DateTime");
+    Rtc.SetDateTime(compiled);
+  } else if (now > compiled) {
+    Serial.println("RTC is newer than compile time, this is expected");
+  } else if (now == compiled) {
+    Serial.println("RTC is the same as compile time, while not expected all is still fine");
+  }
 }
 
 void setup() {
@@ -251,22 +296,27 @@ void setup() {
   lcd.createChar(7, per);
 
   setup_wifi();
+  setup_rtc();
   //check for the presence of the shield
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   Serial.println("Callback!");
+  
   if (client.connect(clientName)) {
-   Serial.println("MQTT Clinet Connect!");
-   subscribeMQTT();
-   Serial.println("Subscribe!");
+    Serial.println("MQTT Clinet Connect!");
+    subscribeMQTT();
+    Serial.println("Subscribe!");
   }
 }
+
 char tem[20];
 char hum[20];
 char illuminance[20];
 char soilhumidity[20];
 char pubJson[512];
+
 void loop() {
+  RtcDateTime now = Rtc.GetDateTime();
   if (!client.connected()) {
     Serial.println("Mqtt Client Not Connected");
     reconnect();
@@ -278,10 +328,10 @@ void loop() {
   utoa(cdsValue, illuminance, 10);
   utoa(waterValue, soilhumidity, 10);
   sprintf(pubJson, "{\"Sensor\":{\"temperature\":%s,\"humidity\":%s,\"illuminance\":%s,\"soilhumidity\":%s},\"Actuator\":{\"led\":%d,\"fan\":%d,\"pump\":%d,\"window\":%d}}",
-           tem, hum, illuminance, soilhumidity,isLed, isFan, isPump, isWindow );
+          tem, hum, illuminance, soilhumidity, isLed, isFan, isPump, isWindow);
   
   send_time += 1;
-  if ((send_time) > 60) {
+  if ((send_time) > 30) {
     Serial.println("Sensor Timer is ON");
     client.publish("smart/farm/message", pubJson, MQTTpubQos);
     Serial.print("temperature : ");
@@ -292,30 +342,28 @@ void loop() {
     Serial.println(cdsValue);
     Serial.print("soilhumidity : ");
     Serial.println(waterValue);
+    Serial.println(__TIME__);
     send_time = 0;
   }
 
   timeout += 1;
-  if (timeout % 60 == 0) {
+  if (timeout % 10 == 0) {
     cdsValue = analogRead(0);
     cdsValue /= 10;
     waterValue = analogRead(1);
     waterValue /= 10;
     humidity = dht.readHumidity();
     temperature = dht.readTemperature();
-    
+
     lcd.clear();
     displayToggle = !displayToggle;
     if (displayToggle == 1) {
       lcd.setCursor(0, 0);
-      lcd.write(5);
-      sprintf(sData, "WELCOME TO FARM");
+      sprintf(sData, "%04u/%02u/%02u", now.Year(), now.Month(), now.Day());
       lcd.print(sData);
       lcd.setCursor(0, 1);
-      sprintf(sData, "FARM WORKING...");
+      sprintf(sData, "%02u:%02u", now.Hour(), now.Minute());
       lcd.print(sData);
-      lcd.write(3);
-      
     } else {
       //온도
       lcd.setCursor(0, 0);
@@ -339,7 +387,7 @@ void loop() {
       lcd.print(":");
       sprintf(sData, "%02d", cdsValue);
       lcd.print(sData);
-      
+
       //습도
       lcd.setCursor(0, 1);
       lcd.write(4);
